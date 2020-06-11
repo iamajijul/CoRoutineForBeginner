37515f26990af5e7efdc364bd08ce23df9c5d7d2
Ajijul Mondal <ajijulmondal786@gmail.com>
2020-06-11 21:44:44 +0400

    Run Blocking method

--- app/src/main/java/com/ajijul/coroutinebyandroiddevs/MainActivity.kt
+++ app/src/main/java/com/ajijul/coroutinebyandroiddevs/MainActivity.kt
@@ -21,38 +21,29 @@ class MainActivity : AppCompatActivity() {
         // Global scope live your CoRoutine until application die or after completion
         //task CoRoutine automatically remove from memory
 
-        GlobalScope.launch {
 
-            delay(3000) // delay() only make pause of current CoRoutine, not whole thread.
-            // Whereas sleep() method in Thread class make pause of whole thread. On
-            // Construction example, if one labour make delay() request then whole construction not
-            // going to cancel but if whole construction got cancel then whole work will stop.
-           println(doNetworkOperation())
-           println(doNetworkOperation2())
-            Log.e(TAG,"Hello from CoRoutine within the thread ${Thread.currentThread().name}")
+        //RunBlocking
+        runBlocking { //its block the current thread
 
+            launch(Dispatchers.IO) {
+                delay(1000L)// its not delaying whole thread, its only delaying
+                // current coroutine
+                println("Delaying first coRoutine")
+            }
+
+            launch(Dispatchers.IO) {
+                delay(1000L)
+                println("Delaying second coRoutine")
+            }
+            println("Before blocking start")
+            delay(6000L)//its delaying whole current thread main, it will freeze the UI
+            println("after blocking end")
 
         }
+        //////////////
 
 
 
     }
 
-    private suspend fun doNetworkOperation(): String {
-
-        delay(3000)
-        return "l"
-    }
-
-    private suspend fun doNetworkOperation2(): String {
-
-        delay(3000)
-        return "m"
-    }
-
-    fun fib(n: Int): Long {
-        return if (n == 0) 0
-        else if (n == 1) 1
-        else fib(n - 1) + fib(n - 2)
-    }
 }
