ced62a2be6c1c13b3d63869248e12af7cb7d44ed
Ajijul Mondal <ajijulmondal786@gmail.com>
2020-06-11 21:41:09 +0400

    CoRoutine Context

--- app/src/main/java/com/ajijul/coroutinebyandroiddevs/MainActivity.kt
+++ app/src/main/java/com/ajijul/coroutinebyandroiddevs/MainActivity.kt
@@ -21,17 +21,18 @@ class MainActivity : AppCompatActivity() {
         // Global scope live your CoRoutine until application die or after completion
         //task CoRoutine automatically remove from memory
 
-        GlobalScope.launch {
 
-            delay(3000) // delay() only make pause of current CoRoutine, not whole thread.
-            // Whereas sleep() method in Thread class make pause of whole thread. On
-            // Construction example, if one labour make delay() request then whole construction not
-            // going to cancel but if whole construction got cancel then whole work will stop.
-           println(doNetworkOperation())
-           println(doNetworkOperation2())
-            Log.e(TAG,"Hello from CoRoutine within the thread ${Thread.currentThread().name}")
-
-
+//        //CoRoutine  context
+        GlobalScope.launch(Dispatchers.IO) {
+            // .Main : for ui/noBlocking operation
+            // .Default : CPU intensive work
+            // .UnContained :
+            //.IO : for network operation
+            // custom(ex: newSingleThreadContext())
+            val answer = doNetworkOperation()
+            withContext(Dispatchers.Main){
+                tvDummy.text = answer
+            }
         }
 
 
@@ -44,15 +45,4 @@ class MainActivity : AppCompatActivity() {
         return "l"
     }
 
-    private suspend fun doNetworkOperation2(): String {
-
-        delay(3000)
-        return "m"
-    }
-
-    fun fib(n: Int): Long {
-        return if (n == 0) 0
-        else if (n == 1) 1
-        else fib(n - 1) + fib(n - 2)
-    }
 }
